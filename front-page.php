11857b278ba3fb6dd7519770b1a8905fa81acc2e
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-07-10 22:12:40 +0200

    Initial revision

---
+++ front-page.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+    <div id="main-content" class="main-content">
+        <div class="row sam-content">
+            <div class="col-md-4">
+                <div class="welcome text-center">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri()?>/image/stern.png" style="width:120px;"/></a>
+                    <h1 style="font-size:3.6em;" class="sam-color-two">Welcome</h1>
+                    <h4 class="sam-color-grey">and thank you for visiting</h4>
+                    <h2 class="sam-color-one">Katrin <span class="sam-color-two">&</span> Sandra</h2>
+                    <h1 style="font-size:2.2em; letter-spacing: 0.2em;" class="sam-color-two sam-font-vibes">photo and film</h1>
+                    <hr/>
+                    <div>
+                        <a href="https://www.facebook.com/katrinandsandra" target="_blank" class="sam-icon-size-48 sam-icon sam-color-two">&#xf106;</a>
+                        <a href="https://plus.google.com/112711029813738652601/posts" target="_blank" class="sam-icon-size-48 sam-icon sam-color-two">&#xf101;</a>
+                        <a href="http://www.pinterest.com/katrinandsandra/" target="_blank" class="sam-icon-size-48 sam-icon sam-color-two">&#xf103;</a>
+                        <a href="https://vimeo.com/katrinandsandra" target="_blank" class="sam-icon-size-48 sam-icon sam-color-two">&#xf104;</a>
+                    </div>
+                    <hr/>
+                    <div class="teaser-box text-left">
+                        <p>
+                            <a href="/about/"><span class="teaser-camera glyphicon glyphicon-camera"></span></a> Katrin und Sandra.
+                        </p><p>
+                            Auf jedenfall zu zweit unterwegs und gemeinsam wollen wir eins: in Bildern eure Geschichten erzählen.
+                        </p>
+                        <p>
+                            Persönlich. Lebendig. Einzigartig wie ihr. Mit unserer langjährigen Erfahrung sind wir eure Begleiter auf dem Weg zur Hochzeit und an eurem großen Tag.
+                            Dabei schaffen wir fotografische Erinnerungen, die noch in Jahrzehnten die großen Momente eurer Liebe lebendig werden lassen.</p>
+                        <p>Es ist eure Hochzeit. Entspannt euch! Wir sind für euch da.</p>
+                        <p class="text-right">Katrin und Sandra.</p>
+                    </div>
+
+                    <div class="teaser-box sam-boxed">
+                        <p>
+                            <a target="_blank" href="http://www.instagram.com/katrinandsandra" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
+                            <a target="_blank" href="http://www.instagram.com/katrinandsandra">Folge <strong>Katrin</strong> auf Instagram</a>
+                        </p>
+                        <p>
+                            <a target="_blank" href="http://www.instagram.com/sandraundkatrin" class="sam-icon-size-48 sam-icon sam-color-two">&#xf102;</a>
+                            <a target="_blank" href="http://www.instagram.com/sandraundkatrin">Folge <strong>Sandra</strong> auf Instagram</a>
+                        </p>
+                    </div>
+                </div>
+            </div> <!-- col-md-4 -->
+            
+            <div class="col-md-8">
+                <div class="row videoWrapper">
+                    <iframe src="http://player.vimeo.com/video/<?php echo get_post_meta(get_the_id(), "video", true); ?>?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0"
+                            frameborder="0" webkitallowfullscreen mozallowfullscreen
+                            allowfullscreen>
+                    </iframe>
+                </div>
+
+                <?php
+                $query = new WP_Query( array (
+                    'post_type' => 'post',
+                    'posts_per_page' => '2',
+                    'category_name' => 'Featured'
+                ));
+
+
+                if ( $query->have_posts() ) :
+                    // Start the Loop.
+                    while ( $query->have_posts() ) : $query->the_post();
+
+                        get_template_part( 'frontpost');
+
+                    endwhile;
+
+                else :
+                    // If no content, include the "No posts found" template.
+                    get_template_part( 'content', 'none' );
+
+                endif;
+                ?>  
+
+            </div> <!-- col-md-8 -->
+
+            <div class="clearfix"></div>
+            <!-- /video and blog -->
+        </div>
+
+        <div class="row sam-section-row text-center">
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div>
+                    <a href="/wedding/">
+                        <img class="center-block img-responsive" src="<?php echo get_template_directory_uri();?>/image/1_Wedding.png"/>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div>
+                    <a href="/blog/">
+                        <img class="center-block img-responsive" src="<?php echo get_template_directory_uri();?>/image/2_Lifestyle.png"/>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div>
+                    <a href="/films/">
+                        <img class="center-block img-responsive" src="<?php echo get_template_directory_uri();?>/image/3_Films.png"/>
+                     </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div>
+                    <a href="/workshops/">
+                        <img class="center-block img-responsive" src="<?php echo get_template_directory_uri();?>/image/4_workshop.png"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+get_footer();
---
+++ frontpost.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+<div class="row sam-blog-content">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="col-md-6 col-sm-6 center-block hero-image">
+
+            <?php the_post_thumbnail(null, array('class' => 'img-responsive')); ?>
+
+        </header>
+        <div class="col-md-6 col-sm-6">
+            <?php the_title( '<h2 class="sam-color-grey"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+            <p class="text-justify">
+
+                <?php the_excerpt( __( 'weiterlesen <span class="meta-nav">&rarr;</span>', 'greyskull14'), true );
+                //the_excerpt();
+                ?>
+            </p>
+        </div>
+    </article>
+</div>
