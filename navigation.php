aa5ccd25e61900a0337e77e2059acd5b351f9e74
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-09-24 22:51:39 +0200

    Add pagination

--- navigation.php
+++ navigation.php
@@ -1,8 +1,13 @@
 <div class="row post-navigation" >
-    <div class="col-md-6">
+    <!-- pager -->
+    <div class="text-center">
+        <?php grayskull_numeric_posts_nav(); ?>
+    </div>
+
+    <!-- <div class="col-md-6">
         <?php next_posts_link('&laquo; Ältere Einträge','') ?>
     </div>
     <div class="col-md-6">
         <span class="pull-right"><?php previous_posts_link('Neuere Einträge &raquo;') ?></span>
-    </div>
+    </div> -->
 </div>
