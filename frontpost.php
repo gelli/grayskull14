6d37e0a2b5482eae7a2655015906057a7cdfe5b4
Christian Gellweiler <christian.gellweiler@1und1.de>
2014-09-24 23:06:21 +0200

    Add link to cover images

--- frontpost.php
+++ frontpost.php
@@ -13,9 +13,7 @@
 <div class="row sam-blog-content">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="col-md-6 col-sm-6 center-block hero-image">
-
-            <?php the_post_thumbnail(null, array('class' => 'img-responsive')); ?>
-
+            <?php echo '<a href="' . esc_url( get_permalink() ) . '">'. get_the_post_thumbnail(null, null, array("class" => "img-responsive")) .'</a>'; ?>
         </header>
         <div class="col-md-6 col-sm-6">
             <?php the_title( '<h2 class="sam-color-grey"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
