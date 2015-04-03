e94e8442fcd3b8abca8e1513977d818cc1ce6b50
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-04-03 19:26:20 +0200

    Fix but where nav and footer content is not contained within the normal content width

--- footer.php
+++ footer.php
@@ -2,53 +2,53 @@
 </div> <!-- container -->
 
 <footer class="container-fluid">
-    <div class="row">
-        <div class="col-md-4 col-sm-4 text-center">
-            <div class="row-centered text-left">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 text-center">
+                <div class="row-centered text-left">
 
-            <h1>Kontakt</h1>
-            <hr/>
-            <p><b>Katrin and Sandra</b></p>
-                <p>
-            Ina-Seidel-Str.7      <br/>
-            76684 Oestringen           </p>
-            <p>
-                <a href="mailto:mail@katrinandsandra.de"><span class="glyphicon glyphicon-envelope"></span> mail@katrinandsandra.de</a><br/>
-                <span class="glyphicon glyphicon-phone-alt"></span> +49 (0)176 88121418
-            </p>
-            <p>
-                <a href="/impressum/">Impressum</a> | <a href="/agb/">AGB</a>
-            </p>
-        </div>
-        </div>
-        <div class="col-md-4 col-sm-4 text-center">
-            <div class="row-centered social-contact">
-                <div class="social-link"><a href="https://www.facebook.com/katrinandsandra" target="_blank"><span class="sam-icon">&#xf106;</span> Like us on facebook</a></div>
-                <div class="social-link"><a href="https://plus.google.com/112711029813738652601/posts" target="_blank">follow us on google <span class="sam-icon">&#xf101;</span></a></div>
-                <div class="social-link"><a href="http://www.pinterest.com/katrinandsandra/" target="_blank"><span class="sam-icon">&#xf103;</span> pin us on pinterest </a></div>
-                <div class="social-link"><a href="http://www.instagram.com/katrinandsandra" target="_blank">Katrin on instagram <span class="sam-icon">&#xf102;</span></a></div>
-                <div class="social-link"><a href="http://www.instagram.com/sandraundkatrin" target="_blank"><span class="sam-icon">&#xf102;</span> Sandra on instagram</a></div>
-                <div class="social-link"><a href="https://vimeo.com/katrinandsandra" target="_blank">watch us on vimeo <span class="sam-icon">&#xf104;</span></a></div>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-4 text-center">
-            <div class="row-centered text-left">
-                <h1>Featured on</h1>
+                <h1>Kontakt</h1>
                 <hr/>
-                <ul>
-                    <li><a target="_blank" href="http://einhochzeitsblog.com/hochzeitsfotos-reportagen-hochzeit-hochzeitsreportagen-hochzeitsinspirationen/eine-hochzeit-am-zuerichsee-und-warum-ich-verdammt-viel-anker-in-den-elbstrand-zementiere/">Ein Hochzeitsblog</a></li>
-                    <li><a target="_blank" href="http://www.friedatheres.com/ameli-und-martin-hochzeit">Frieda Therés</a></li>
-                    <li><a target="_blank" href="http://www.fraeulein-k-sagt-ja.de/allgemein/schon-wieder-ein-videoabend-aber-ja-doch">Fräulein K. sagt ja</a></li>
-                    <li><a target="_blank" href="http://www.brautstories.de/2013/10/14/mirella-michael-hochzeit-in-frankfurt-mit-katrinsandra-photographers/">Brautstories</a></li>
-                </ul>
+                <p><b>Katrin and Sandra</b></p>
+                    <p>
+                Ina-Seidel-Str.7      <br/>
+                76684 Oestringen           </p>
+                <p>
+                    <a href="mailto:mail@katrinandsandra.de"><span class="glyphicon glyphicon-envelope"></span> mail@katrinandsandra.de</a><br/>
+                    <span class="glyphicon glyphicon-phone-alt"></span> +49 (0)176 88121418
+                </p>
+                <p>
+                    <a href="/impressum/">Impressum</a> | <a href="/agb/">AGB</a>
+                </p>
+            </div>
+            </div>
+            <div class="col-md-4 col-sm-4 text-center">
+                <div class="row-centered social-contact">
+                    <div class="social-link"><a href="https://www.facebook.com/katrinandsandra" target="_blank"><span class="sam-icon">&#xf106;</span> Like us on facebook</a></div>
+                    <div class="social-link"><a href="https://plus.google.com/112711029813738652601/posts" target="_blank">follow us on google <span class="sam-icon">&#xf101;</span></a></div>
+                    <div class="social-link"><a href="http://www.pinterest.com/katrinandsandra/" target="_blank"><span class="sam-icon">&#xf103;</span> pin us on pinterest </a></div>
+                    <div class="social-link"><a href="http://www.instagram.com/katrinandsandra" target="_blank">Katrin on instagram <span class="sam-icon">&#xf102;</span></a></div>
+                    <div class="social-link"><a href="http://www.instagram.com/sandraundkatrin" target="_blank"><span class="sam-icon">&#xf102;</span> Sandra on instagram</a></div>
+                    <div class="social-link"><a href="https://vimeo.com/katrinandsandra" target="_blank">watch us on vimeo <span class="sam-icon">&#xf104;</span></a></div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 text-center">
+                <div class="row-centered text-left">
+                    <h1>Featured on</h1>
+                    <hr/>
+                    <ul>
+                        <li><a target="_blank" href="http://einhochzeitsblog.com/hochzeitsfotos-reportagen-hochzeit-hochzeitsreportagen-hochzeitsinspirationen/eine-hochzeit-am-zuerichsee-und-warum-ich-verdammt-viel-anker-in-den-elbstrand-zementiere/">Ein Hochzeitsblog</a></li>
+                        <li><a target="_blank" href="http://www.friedatheres.com/ameli-und-martin-hochzeit">Frieda Therés</a></li>
+                        <li><a target="_blank" href="http://www.fraeulein-k-sagt-ja.de/allgemein/schon-wieder-ein-videoabend-aber-ja-doch">Fräulein K. sagt ja</a></li>
+                        <li><a target="_blank" href="http://www.brautstories.de/2013/10/14/mirella-michael-hochzeit-in-frankfurt-mit-katrinsandra-photographers/">Brautstories</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
 
         <?php // get_sidebar( 'footer') ?>
-
-    </footer>
-
-    <?php wp_footer(); ?>
-    </body>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
