efd27907b452e83c7bcf9cdd905c64d4e9f51d6b
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-03-02 21:57:51 +0100

    Add Cookie message

--- footer.php
+++ footer.php
@@ -49,5 +49,18 @@
     </div>
 </footer>
 <?php wp_footer(); ?>
+
+<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+<script type="text/javascript">
+    window.cookieconsent_options = {
+      "message":"Um Dir ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch deinen Besuch stimmst Du unserer Verwendung von Cookies zu.",
+      "dismiss":"Alles klar!",
+      "learnMore":"Mehr Informationen",
+      "link":"/impressum","theme": "dark-top"};
+</script>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
+<!-- End Cookie Consent plugin -->
+
 </body>
 </html>
