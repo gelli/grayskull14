5055db279baf3635ec496af422bf312e2aefe406
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-03-02 22:42:56 +0100

    use light theme for cookie warning

--- footer.php
+++ footer.php
@@ -53,10 +53,10 @@
 <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
 <script type="text/javascript">
     window.cookieconsent_options = {
-      "message":"Um Dir ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch deinen Besuch stimmst Du unserer Verwendung von Cookies zu.",
+      "message":"Um Dir ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies.",
       "dismiss":"Alles klar!",
       "learnMore":"Mehr Informationen",
-      "link":"/impressum","theme": "dark-top"};
+      "link":"/impressum","theme": "light-top"};
 </script>
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
