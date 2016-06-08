76c7317792519da2aa9e2bd283f27ac01f1c14e5
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-06-08 21:50:48 +0200

    Fix spacing in mobile (xs) view

--- sidebar.php
+++ sidebar.php
@@ -8,9 +8,10 @@
  */
 ?>
 <div id="sidebar" role="complementary">
-    <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-        <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-            <?php dynamic_sidebar( 'sidebar-1' ); ?>
-        </div><!-- #primary-sidebar -->
-    <?php endif; ?>
+  <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+      <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+          <?php dynamic_sidebar( 'sidebar-1' ); ?>
+      </div><!-- #primary-sidebar -->
+  <?php endif; ?>
+
 </div><!-- #secondary -->
